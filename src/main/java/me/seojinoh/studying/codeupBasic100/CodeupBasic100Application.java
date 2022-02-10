69f2d88c97a306c70ae827190c9662e3a730d44f
Seojin Oh <seojinoh.me@gmail.com>
2022-02-10 16:05:56 +0900

    feat : Add Spring Boot framework project

---
+++ src/main/java/me/seojinoh/studying/codeupBasic100/CodeupBasic100Application.java
@@ -0,0 +1,13 @@
+package me.seojinoh.studying.codeupBasic100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeupBasic100Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeupBasic100Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/seojinoh/studying/codeupBasic100/CodeupBasic100ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.seojinoh.studying.codeupBasic100;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeupBasic100ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
