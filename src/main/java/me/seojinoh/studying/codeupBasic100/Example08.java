a1dfed282d8b93c46cd49f35a38f360db6419f8d
Seojin Oh <seojinoh.me@gmail.com>
2022-02-23 21:51:36 +0900

    feat : Add Example08

---
+++ src/main/java/me/seojinoh/studying/codeupBasic100/Example08.java
@@ -0,0 +1,9 @@
+package me.seojinoh.studying.codeupBasic100;
+
+public class Example08 implements IExample {
+
+	public void run() {
+		System.out.println("\u250c\u252c\u2510\n\u251c\u253c\u2524\n\u2514\u2534\u2518");
+	}
+
+}
