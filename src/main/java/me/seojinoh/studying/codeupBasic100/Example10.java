443fb38e48e60d4bfec0e76287296129a4ca028c
Seojin Oh <seojinoh.me@gmail.com>
2022-02-23 21:51:49 +0900

    feat : Add Example10

---
+++ src/main/java/me/seojinoh/studying/codeupBasic100/Example10.java
@@ -0,0 +1,17 @@
+package me.seojinoh.studying.codeupBasic100;
+
+import java.util.Scanner;
+
+public class Example10 implements IExample {
+
+	public void run() {
+		Scanner scanner = new Scanner(System.in, "UTF-8");
+
+		int n;
+
+		n = scanner.nextInt();
+
+		System.out.println(n);
+	}
+
+}
