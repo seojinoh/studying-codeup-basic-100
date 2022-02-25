db97361750720c30a517c63922b09f2a2c5c8362
Seojin Oh <seojinoh.me@gmail.com>
2022-02-25 23:21:32 +0900

    style : Simplify Example13

--- src/main/java/me/seojinoh/studying/codeupBasic100/Example13.java
+++ src/main/java/me/seojinoh/studying/codeupBasic100/Example13.java
@@ -5,12 +5,10 @@ import java.util.Scanner;
 public class Example13 implements IExample {
 
 	public void run() {
-		int a, b;
-
 		Scanner scanner = new Scanner(System.in, "UTF-8");
 
-		a = scanner.nextInt();
-		b = scanner.nextInt();
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
 
 		System.out.println(a + " " + b);
 	}
