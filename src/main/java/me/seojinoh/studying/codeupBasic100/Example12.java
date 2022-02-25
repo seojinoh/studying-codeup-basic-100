e1a2fa9b6e042ebe21d1c5869f869fdaff034677
Seojin Oh <seojinoh.me@gmail.com>
2022-02-25 23:21:29 +0900

    style : Simplify Example12

--- src/main/java/me/seojinoh/studying/codeupBasic100/Example12.java
+++ src/main/java/me/seojinoh/studying/codeupBasic100/Example12.java
@@ -5,11 +5,9 @@ import java.util.Scanner;
 public class Example12 implements IExample {
 
 	public void run() {
-		float f;
-
 		Scanner scanner = new Scanner(System.in, "UTF-8");
 
-		f = scanner.nextFloat();
+		float f = scanner.nextFloat();
 
 		System.out.println(String.format("%.6f", f));
 	}
