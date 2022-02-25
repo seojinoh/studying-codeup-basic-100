738a860af0f941b498d1ee020045879d0074e3cd
Seojin Oh <seojinoh.me@gmail.com>
2022-02-25 23:21:26 +0900

    style : Simplify Example11

--- src/main/java/me/seojinoh/studying/codeupBasic100/Example11.java
+++ src/main/java/me/seojinoh/studying/codeupBasic100/Example11.java
@@ -5,11 +5,9 @@ import java.util.Scanner;
 public class Example11 implements IExample {
 
 	public void run() {
-		char c;
-
 		Scanner scanner = new Scanner(System.in, "UTF-8");
 
-		c = scanner.next().charAt(0);
+		char c = scanner.next().charAt(0);
 
 		System.out.println(c);
 	}
