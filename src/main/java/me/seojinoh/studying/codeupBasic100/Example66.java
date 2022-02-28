c3fa0ff59cd0331e063fb63d17b427a9e80100fe
Seojin Oh <seojinoh.me@gmail.com>
2022-02-28 16:16:32 +0900

    feat : Add Example66

---
+++ src/main/java/me/seojinoh/studying/codeupBasic100/Example66.java
@@ -0,0 +1,21 @@
+package me.seojinoh.studying.codeupBasic100;
+
+import java.util.Scanner;
+
+public class Example66 implements IExample {
+
+	public void run() {
+		Scanner scanner = new Scanner(System.in, "UTF-8");
+
+		String[] inputs = scanner.nextLine().split("\\s", 3);
+
+		for(String input : inputs) {
+			if(Integer.parseInt(input) % 2 == 0) {
+				System.out.println("even");
+			} else {
+				System.out.println("odd");
+			}
+		}
+	}
+
+}
