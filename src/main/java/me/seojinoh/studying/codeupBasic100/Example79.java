29276543b142d69f9ea1695af571b3e41b0317c7
Seojin Oh <seojinoh.me@gmail.com>
2022-02-28 18:25:41 +0900

    feat : Add Example79

---
+++ src/main/java/me/seojinoh/studying/codeupBasic100/Example79.java
@@ -0,0 +1,21 @@
+package me.seojinoh.studying.codeupBasic100;
+
+import java.util.Scanner;
+
+public class Example79 implements IExample {
+
+	public void run() {
+		Scanner scanner = new Scanner(System.in, "UTF-8");
+
+		while(true) {
+			char input = scanner.next().toCharArray()[0];
+
+			System.out.println(input);
+
+			if(input == 'q') {
+				break;
+			}
+		}
+	}
+
+}
