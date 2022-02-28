e0f3596ac693c2cf2eba26448872b5b6ab323aaa
Seojin Oh <seojinoh.me@gmail.com>
2022-02-28 17:00:52 +0900

    feat : Add Example72

---
+++ src/main/java/me/seojinoh/studying/codeupBasic100/Example72.java
@@ -0,0 +1,22 @@
+package me.seojinoh.studying.codeupBasic100;
+
+import java.util.Scanner;
+
+public class Example72 implements IExample {
+
+	public void run() {
+		Scanner scanner = new Scanner(System.in, "UTF-8");
+
+		int count = scanner.nextInt();
+		int[] inputs = new int[count];
+
+		for(int i = 0; i < count; i++) {
+			inputs[i] = scanner.nextInt();
+		}
+
+		for(int input : inputs) {
+			System.out.println(input);
+		}
+	}
+
+}
