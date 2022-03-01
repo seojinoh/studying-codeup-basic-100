f390fe6f4d514586fbc02979a0a2db9dbe482bb7
Seojin Oh <seojinoh.me@gmail.com>
2022-03-01 09:47:52 +0900

    feat : Add Example88

---
+++ src/main/java/me/seojinoh/studying/codeupBasic100/Example88.java
@@ -0,0 +1,23 @@
+package me.seojinoh.studying.codeupBasic100;
+
+import java.util.Scanner;
+
+public class Example88 implements IExample {
+
+	public void run() {
+		Scanner scanner = new Scanner(System.in, "UTF-8");
+
+		int input = scanner.nextInt();
+
+		String output = "";
+
+		for(int i = 1; i < input + 1; i++) {
+			if(i % 3 != 0) {
+				output += i + " ";
+			}
+		}
+
+		System.out.println(output);
+	}
+
+}
