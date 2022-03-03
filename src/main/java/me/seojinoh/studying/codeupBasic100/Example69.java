10ae9cee5bce1afae92225a4f9ae2f7e17dd2980
Seojin Oh <seojinoh.me@gmail.com>
2022-03-03 16:38:20 +0900

    feat : Update Example69

--- src/main/java/me/seojinoh/studying/codeupBasic100/Example69.java
+++ src/main/java/me/seojinoh/studying/codeupBasic100/Example69.java
@@ -7,7 +7,7 @@ public class Example69 implements IExample {
 	public void run() {
 		Scanner scanner = new Scanner(System.in, "UTF-8");
 
-		char input = scanner.next().toCharArray()[0];
+		char input = scanner.next().charAt(0);
 
 		switch(input) {
 		case 'A':
