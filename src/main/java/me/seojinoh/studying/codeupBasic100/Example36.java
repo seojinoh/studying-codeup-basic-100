5e14c986e1599c642c11cfc7200d86c435962d3d
Seojin Oh <seojinoh.me@gmail.com>
2022-03-03 16:38:12 +0900

    feat : Update Example36

--- src/main/java/me/seojinoh/studying/codeupBasic100/Example36.java
+++ src/main/java/me/seojinoh/studying/codeupBasic100/Example36.java
@@ -7,7 +7,7 @@ public class Example36 implements IExample {
 	public void run() {
 		Scanner scanner = new Scanner(System.in, "UTF-8");
 
-		char input = scanner.next().toCharArray()[0];
+		char input = scanner.next().charAt(0);
 
 		System.out.println((int) input);
 	}
